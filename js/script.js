a3213fa3e3777e1c097df95774023a15ca415fa4
Zichao <cafeboss@live.cn>
2016-09-16 14:53:40 +0800

    Shakes and omar; improve code structure

--- js/script.js
+++ js/script.js
@@ -1,102 +1,143 @@
+var audi_left_center = 3.5;
+var audi_left_left = 8;
+var audi_mid_top = 4;
+var table_height = 0.8 / 2;
+var chair_height = 0.5 / 2;
+var audi_other_z = -5.5;
+var audi_other_x = 2.2;
+var chair_sep = 0.65;
+
+
 var audi_settings = 
-	'<a-box class="box" color="tomato" position="-3 0.4 -4" depth="1.52" height="0.8" width="0.8"></a-box>' + 
-	'<a-box class="box" color="tomato" position="-3 0.4 0" depth="1.52" height="0.8" width="0.8"></a-box>' + 
-	'<a-box class="box" color="tomato" position="-3 0.4 4" depth="1.52" height="0.8" width="0.8"></a-box>' +
-	'<a-box class="box" color="tomato" position="3 0.4 -4" depth="1.52" height="0.8" width="0.8"></a-box>' +
-	'<a-box class="box" color="tomato" position="3 0.4 0" depth="1.52" height="0.8" width="0.8"></a-box>' +
-	'<a-box class="box" color="tomato" position="3 0.4 4" depth="1.52" height="0.8" width="0.8"></a-box>' +
+	'<a-box class="box" mixin="table table_rotated" position="-' + audi_left_center + ' 0.4 -' + audi_mid_top + '"></a-box>' + 
+	'<a-box class="box" mixin="table table_rotated" position="-' + audi_left_center + ' 0.4 0"></a-box>' + 
+	'<a-box class="box" mixin="table table_rotated" position="-' + audi_left_center + ' 0.4 ' + audi_mid_top + '"></a-box>' +
+	'<a-box class="box" mixin="table table_rotated" position="' + audi_left_center + ' 0.4 -' + audi_mid_top + '"></a-box>' +
+	'<a-box class="box" mixin="table table_rotated" position="' + audi_left_center + ' 0.4 0"></a-box>' +
+	'<a-box class="box" mixin="table table_rotated" position="' + audi_left_center + ' 0.4 ' + audi_mid_top + '"></a-box>' +
 	'<a-box class="box" color="pink" position="0 1.225 -1.75" depth="3.5" height="2.45" width="3.5"></a-box>' +
 	'<a-plane color="#AAA" position="-7.5 1.05 -9" width="2" height="2.1"></a-plane>' +
 	'<a-plane color="#AAA" position="7.5 1.05 -9" width="2" height="2.1"></a-plane>' +
-	//chair
-	'<a-box class="box" color="orange" position="-4 0.25 -5" depth="0.5" height="0.5" width="0.5"></a-box>' +
-	'<a-box class="box" color="orange" position="-4 0.25 -4" depth="0.5" height="0.5" width="0.5"></a-box>' +
-	'<a-box class="box" color="orange" position="-4 0.25 -3" depth="0.5" height="0.5" width="0.5"></a-box>' +
-	'<a-box class="box" color="orange" position="-3 0.25 -5.5" depth="0.5" height="0.5" width="0.5"></a-box>' +
-	'<a-box class="box" color="orange" position="-3 0.25 -2.5" depth="0.5" height="0.5" width="0.5"></a-box>' +
-	//chair
-	'<a-box class="box" color="orange" position="-4 0.25 -1" depth="0.5" height="0.5" width="0.5"></a-box>' +
-	'<a-box class="box" color="orange" position="-4 0.25 0" depth="0.5" height="0.5" width="0.5"></a-box>' +
-	'<a-box class="box" color="orange" position="-4 0.25 1" depth="0.5" height="0.5" width="0.5"></a-box>' +
-	'<a-box class="box" color="orange" position="-3 0.25 -1.5" depth="0.5" height="0.5" width="0.5"></a-box>' +
-	'<a-box class="box" color="orange" position="-3 0.25 1.5" depth="0.5" height="0.5" width="0.5"></a-box>' +
-	//chair
-	'<a-box class="box" color="orange" position="-4 0.25 5" depth="0.5" height="0.5" width="0.5"></a-box>' +
-	'<a-box class="box" color="orange" position="-4 0.25 4" depth="0.5" height="0.5" width="0.5"></a-box>' +
-	'<a-box class="box" color="orange" position="-4 0.25 3" depth="0.5" height="0.5" width="0.5"></a-box>' +
-	'<a-box class="box" color="orange" position="-3 0.25 5.5" depth="0.5" height="0.5" width="0.5"></a-box>' +
-	'<a-box class="box" color="orange" position="-3 0.25 2.5" depth="0.5" height="0.5" width="0.5"></a-box>' +
-	//chair
-	'<a-box class="box" color="orange" position="4 0.25 -5" depth="0.5" height="0.5" width="0.5"></a-box>' +
-	'<a-box class="box" color="orange" position="4 0.25 -4" depth="0.5" height="0.5" width="0.5"></a-box>' +
-	'<a-box class="box" color="orange" position="4 0.25 -3" depth="0.5" height="0.5" width="0.5"></a-box>' +
-	'<a-box class="box" color="orange" position="3 0.25 -5.5" depth="0.5" height="0.5" width="0.5"></a-box>' +
-	'<a-box class="box" color="orange" position="3 0.25 -2.5" depth="0.5" height="0.5" width="0.5"></a-box>' +
-	//chair
-	'<a-box class="box" color="orange" position="4 0.25 -1" depth="0.5" height="0.5" width="0.5"></a-box>' +
-	'<a-box class="box" color="orange" position="4 0.25 0" depth="0.5" height="0.5" width="0.5"></a-box>' +
-	'<a-box class="box" color="orange" position="4 0.25 1" depth="0.5" height="0.5" width="0.5"></a-box>' +
-	'<a-box class="box" color="orange" position="3 0.25 -1.5" depth="0.5" height="0.5" width="0.5"></a-box>' +
-	'<a-box class="box" color="orange" position="3 0.25 1.5" depth="0.5" height="0.5" width="0.5"></a-box>' +
-	//chair
-	'<a-box class="box" color="orange" position="4 0.25 5" depth="0.5" height="0.5" width="0.5"></a-box>' +
-	'<a-box class="box" color="orange" position="4 0.25 4" depth="0.5" height="0.5" width="0.5"></a-box>' +
-	'<a-box class="box" color="orange" position="4 0.25 3" depth="0.5" height="0.5" width="0.5"></a-box>' +
-	'<a-box class="box" color="orange" position="3 0.25 5.5" depth="0.5" height="0.5" width="0.5"></a-box>' +
-	'<a-box class="box" color="orange" position="3 0.25 2.5" depth="0.5" height="0.5" width="0.5"></a-box>';
+	//chair left
+	fiveChairs(-audi_left_left, audi_mid_top, 90) +
+	fiveChairs(-audi_left_left, 0, 90) +
+	fiveChairs(-audi_left_left, -audi_mid_top, 90) +
+	//Chair right upper
+	fiveChairs(audi_left_left, audi_mid_top, 90) +
+	fiveChairs(audi_left_left, 0, 90) +
+	fiveChairs(audi_left_left, -audi_mid_top, 90) +
+	//chairs right
+	coupleChairs(audi_left_center - 0.3, audi_mid_top + 0.8, 0) +
+	coupleChairs(audi_left_center - 0.3, 0.8, 0) +
+	coupleChairs(audi_left_center - 0.3, -audi_mid_top + 0.8, 0) +
+	//chairs left
+	coupleChairs(-audi_left_center - 0.3, audi_mid_top + 0.8, 0) +
+	coupleChairs(-audi_left_center - 0.3, 0.8, 0) +
+	coupleChairs(-audi_left_center - 0.3, -audi_mid_top + 0.8, 0) +
+	//chairs
+	fiveChairs(audi_other_x, audi_other_z, 0);
+
+var badm_center_x = 2.5;
+var badm_center_z = 3.0;
+var badm_right_right = 5.5;
+var badm_upper_right = 3.5;
+var badm_corner_z = -6.5;
+var badm_upper_z = -8;
+var badm_upper_mid = 0.5;
+var badm_upper_lower = -6;
 
 var badm_settings = 
-	'<a-box class="box" color="tomato" position="5.5 0.4 0" depth="0.8" height="0.8" width="1.52"></a-box>' + 
-	'<a-box class="box" color="tomato" position="5.5 0.4 -2.3" depth="0.8" height="0.8" width="1.52"></a-box>' + 
-	'<a-box class="box" color="tomato" position="5.5 0.4 2.3" depth="0.8" height="0.8" width="1.52"></a-box>' +
-	'<a-box class="box" color="tomato" position="2 0.4 -8" depth="1.52" height="0.8" width="0.8"></a-box>' +
-	'<a-box class="box" color="tomato" position="0 0.4 -8" depth="1.52" height="0.8" width="0.8"></a-box>' +
-	'<a-box class="box" color="tomato" position="-5.5 0.4 -2" depth="0.8" height="0.8" width="1.52"></a-box>' +
-	'<a-box class="box" color="orange" position="0 0.1 8" depth="2" height="0.2" width="2"></a-box>' +
+	'<a-box class="box" mixin="table table_rotated" position="' + badm_right_right + ' ' + table_height + ' 0"></a-box>' + 
+	'<a-box class="box" mixin="table table_rotated" position="' + badm_right_right + ' ' + table_height + ' ' + String(-badm_center_z) + '"></a-box>' + 
+	'<a-box class="box" mixin="table table_rotated" position="' + badm_right_right + ' ' + table_height + ' ' + badm_center_z + '"></a-box>' +
+	'<a-box class="box" mixin="table" position="' + badm_upper_right + ' ' + table_height + ' ' + badm_upper_z + '"></a-box>' +
+	'<a-box class="box" mixin="table" position="' + badm_upper_mid + ' ' + table_height + ' ' + badm_upper_z + '"></a-box>' +
+	'<a-box class="box" mixin="table table_rotated" position="' + String(-badm_right_right) + ' ' + table_height + ' -2"></a-box>' +
+	'<a-box class="box" color="#fbd" position="0 0.1 8" depth="2" height="0.2" width="2"></a-box>' +
 	'<a-box class="box" color="pink" position="-4.95 1.2 -6.75" depth="3.5" height="2.45" width="3.5"></a-box>' +
 	'<a-plane color="#AAA" position="6.7 1.05 5.5" width="2" height="2.1" rotation="0 -90 0"></a-plane>' +
 	'<a-plane color="#AAA" position="6.7 1.05 -5.5" width="2" height="2.1" rotation="0 -90 0"></a-plane>' +
 	'<a-plane color="#AAA" position="-6.7 1.05 0" width="2" height="2.1" rotation="0 90 0"></a-plane>' +
-	//chair
-	'<a-box class="box" color="yellow" position="6.2 0.25 -3.45" depth="0.5" height="0.5" width="0.5"></a-box>' +
-	'<a-box class="box" color="yellow" position="5.4 0.25 -3.45" depth="0.5" height="0.5" width="0.5"></a-box>' +
-	'<a-box class="box" color="yellow" position="4.6 0.25 -3.45" depth="0.5" height="0.5" width="0.5"></a-box>' +
-	//chair
-	'<a-box class="box" color="yellow" position="6.2 0.25 -1.15" depth="0.5" height="0.5" width="0.5"></a-box>' +
-	'<a-box class="box" color="yellow" position="5.4 0.25 -1.15" depth="0.5" height="0.5" width="0.5"></a-box>' +
-	'<a-box class="box" color="yellow" position="4.6 0.25 -1.15" depth="0.5" height="0.5" width="0.5"></a-box>' +
-	//chair
-	'<a-box class="box" color="yellow" position="6.2 0.25 1.15" depth="0.5" height="0.5" width="0.5"></a-box>' +
-	'<a-box class="box" color="yellow" position="5.4 0.25 1.15" depth="0.5" height="0.5" width="0.5"></a-box>' +
-	'<a-box class="box" color="yellow" position="4.6 0.25 1.15" depth="0.5" height="0.5" width="0.5"></a-box>' +
-	//chair
-	'<a-box class="box" color="yellow" position="6.2 0.25 3.45" depth="0.5" height="0.5" width="0.5"></a-box>' +
-	'<a-box class="box" color="yellow" position="5.4 0.25 3.45" depth="0.5" height="0.5" width="0.5"></a-box>' +
-	'<a-box class="box" color="yellow" position="4.6 0.25 3.45" depth="0.5" height="0.5" width="0.5"></a-box>' +
-	//chair
-	'<a-box class="box" color="yellow" position="4.3 0.25 -2.3" depth="0.5" height="0.5" width="0.5"></a-box>' +
-	'<a-box class="box" color="yellow" position="4.3 0.25 0" depth="0.5" height="0.5" width="0.5"></a-box>' +
-	'<a-box class="box" color="yellow" position="4.3 0.25 2.3" depth="0.5" height="0.5" width="0.5"></a-box>' +
-	//chair
-	'<a-box class="box" color="yellow" position="1 0.25 -8.5" depth="0.5" height="0.5" width="0.5"></a-box>' +
-	'<a-box class="box" color="yellow" position="1 0.25 -7.5" depth="0.5" height="0.5" width="0.5"></a-box>' +
-	//chair
-	'<a-box class="box" color="yellow" position="-1 0.25 -8.8" depth="0.5" height="0.5" width="0.5"></a-box>' +
-	'<a-box class="box" color="yellow" position="-1 0.25 -8" depth="0.5" height="0.5" width="0.5"></a-box>' +
-	'<a-box class="box" color="yellow" position="-1 0.25 -7.2" depth="0.5" height="0.5" width="0.5"></a-box>' +
-	//chair
-	'<a-box class="box" color="yellow" position="3 0.25 -8.8" depth="0.5" height="0.5" width="0.5"></a-box>' +
-	'<a-box class="box" color="yellow" position="3 0.25 -8" depth="0.5" height="0.5" width="0.5"></a-box>' +
-	'<a-box class="box" color="yellow" position="3 0.25 -7.2" depth="0.5" height="0.5" width="0.5"></a-box>' +
-	//chair
-	'<a-box class="box" color="yellow" position="0 0.25 -6.65" depth="0.5" height="0.5" width="0.5"></a-box>' + 
-	'<a-box class="box" color="yellow" position="2 0.25 -6.65" depth="0.5" height="0.5" width="0.5"></a-box>' +
-	//chair
-	'<a-box class="box" color="yellow" position="-6.5 0.25 -3" depth="0.5" height="0.5" width="0.5"></a-box>' +
-	'<a-box class="box" color="yellow" position="-5.5 0.25 -3" depth="0.5" height="0.5" width="0.5"></a-box>' +
-	'<a-box class="box" color="yellow" position="-4.5 0.25 -3" depth="0.5" height="0.5" width="0.5"></a-box>' +
-	'<a-box class="box" color="yellow" position="-4.3 0.25 -2" depth="0.5" height="0.5" width="0.5"></a-box>' +
-	'<a-box class="box" color="yellow" position="-4.7 0.25 -1.1" depth="0.5" height="0.5" width="0.5"></a-box>';
+	//chair right
+	coupleChairs(badm_right_right - 0.2, badm_center_z + 0.85, 0) +
+	coupleChairs(badm_right_right - 0.2, 0.85, 0) +
+	coupleChairs(badm_right_right - 0.2, -badm_center_z + 0.85, 0) +
+	//chairs middle
+	fiveChairs(badm_center_x, -badm_center_z - 0.2, 90) + 
+	fiveChairs(badm_center_x, 0, 90) + 
+	fiveChairs(badm_center_x, badm_center_z + 0.2, 90) + 
+	//Chairs corner
+	fiveChairs(badm_right_right, badm_corner_z, 90) + 
+	//Chairs upper
+	coupleChairs(badm_upper_mid + 0.85, badm_upper_z + 0.2, 90) +
+	coupleChairs(badm_upper_right + 0.85, badm_upper_z + 0.2, 90) +
+	//Chair Upper lower
+	fiveChairs(badm_upper_mid, badm_upper_lower, 0) +
+	fiveChairs(-2.0, -4, 45) + 
+	//Chair left
+	coupleChairs(-badm_right_right + 0.2, -2.85, 180) +
+	fiveChairs(-4, -2, 90) +
+	'<a-box position="-4.2 0.1 6.65" width="5" height="0.2" depth="5" color="lightblue"></a-box>' +
+	'<a-box position="-5.7 0.1 3" width="2" height="0.2" depth="2" color="#ff6"></a-box>';
+
+var shakes_lower_z = 4;
+var shakes_right_x = 1.5;
+var shakes_left_x = 2;
+var shakes_door_z = 5;
+
+var shakes_settings = table(-shakes_left_x, shakes_lower_z, 90) + 
+					  table(-shakes_left_x, 0, 90) +
+					  table(-shakes_left_x, -shakes_lower_z, 90) +
+					  fiveChairs(shakes_right_x, shakes_lower_z, 90) +
+					  fiveChairs(shakes_right_x, 0, 90) +
+					  fiveChairs(shakes_right_x, -shakes_lower_z, 90) +
+					  door(4.65, shakes_door_z, 90) +
+					  door(4.65, -shakes_door_z, 90);
+
+var omar_lower_z = 1.5;
+var omar_right_x = 1.5;
+
+var omar_settings = table(-shakes_left_x, 3 * omar_lower_z, 90) +
+					table(-shakes_left_x, omar_lower_z, 90) +
+					table(-shakes_left_x, -omar_lower_z, 90) +
+					fiveChairs(shakes_right_x, 3 * omar_lower_z, 90) +
+				  	fiveChairs(shakes_right_x - 0.65, omar_lower_z, 90) +
+				  	fiveChairs(shakes_right_x, -omar_lower_z, 90) +
+				  	door(4.65, shakes_door_z, 90) +
+				  	door(4.65, -shakes_door_z, 90) +
+					'<a-box position="-3.65 0.1 -5.2" width="2" height="0.2" depth="2" color="#ff6"></a-box>';
+
+function fiveChairs(x, z, r) {
+	return '<a-entity rotation="0 ' + r + ' 0" position="' + x + ' ' + chair_height + ' ' + z + '">' +
+		   '<a-box class="box" mixin="chair" position="0 0 0"></a-box>' +
+		   '<a-box class="box" mixin="chair" position="' + String(-chair_sep) + ' 0 0"></a-box>' +
+		   '<a-box class="box" mixin="chair" position="' + String(-2 * chair_sep) + ' 0 0"></a-box>' +
+		   '<a-box class="box" mixin="chair" position="' + String(chair_sep) + ' 0 0"></a-box>' +
+		   '<a-box class="box" mixin="chair" position="' + String(2 * chair_sep) + ' 0 0"></a-box>' +
+		   '</a-entity>';
+}
+
+function coupleChairs(x, z, r) {
+	return '<a-entity rotation="0 ' + r + ' 0" position="' + x + ' ' + chair_height + ' ' + z + '">' +
+		   '<a-box class="box" mixin="chair" position="0 0 0"></a-box>' +
+		   '<a-box class="box" mixin="chair" position="' + String(+chair_sep) + ' 0 0"></a-box>' +
+		   '</a-entity>';
+}
+
+function table(x, z, r) {
+	return '<a-box class="box" mixin="table" rotation="0 ' + r + ' 0" position="' + x + ' ' + table_height + ' ' + z + '"></a-box>' +
+			coupleChairs(x - 0.51, z - 0.85, 0);
+}
+
+function door(x, z, r) {
+	var door_height = 2.1 / 2;
+	return '<a-plane mixin="door" position="' + x + ' ' + door_height + ' ' + z + '" rotation="0 ' + r + ' 0"></a-plane>';
+}
+
+$('button').click(function() {
+	$('.focused').removeClass('focused');
+	$(this).addClass('focused');
+});
 
 $('#audi').click(function() {
 	$('#boxes').html(audi_settings);
@@ -112,6 +153,22 @@ $('#badm').click(function() {
 	var y = $('#camera').attr('rotation').y;
 	rotateCompass(y);
 	changeRoomSize(18.3, 13.4);
+});
+
+$('#shakes').click(function() {
+	$('#boxes').html(shakes_settings);
+	envRotation = -90;
+	var y = $('#camera').attr('rotation').y;
+	rotateCompass(y);
+	changeRoomSize(12.38, 9.3);
+});
+
+$('#omar').click(function() {
+	$('#boxes').html(omar_settings);
+	envRotation = -90;
+	var y = $('#camera').attr('rotation').y;
+	rotateCompass(y);
+	changeRoomSize(12.38, 9.3);
 });
 
 function changeRoomSize(len, wid) {
@@ -143,7 +200,7 @@ $('#add').click(function() {
 		alert('Please fill in the dimention with values greater than 0.');
 	} else {
 		var pos = '0 5 0';
-		var box = '<a-box class="box" color="tomato" position="' + pos + '" depth="' + l + '" height="' + h + '" width="' + w + '"></a-box>';
+		var box = '<a-box class="box" mixin="table" position="' + pos + '" depth="' + l + '" height="' + h + '" width="' + w + '"></a-box>';
 		$('#boxes').append(box);
 		$('#dim').val('');
 	}
@@ -189,6 +246,8 @@ function rotateCompass(deg) {
 	var compass = document.getElementById('compass');
 	deg += envRotation;
 	compass.style.webkitTransform = 'rotate(' + deg + 'deg)';
+	compass.style.mozTransform = 'rotate(' + deg + 'deg)';
+	compass.style.msTransform = 'rotate(' + deg + 'deg)';
 }
 
 function canvasLoop(event) {
@@ -213,7 +272,7 @@ function lockChangeHandler() {
 function gridsUp(length, width) {
 	$('#grids').html("");
 	for (var i = -length/2.0 + 0.5; i < length/2.0; i+=0.5) {
-		var hori = '<a-plane class="grid" color="white" material="emissive: white" width="' + width + '" height="0.2" rotation="-90 0 0" position="0 -0.02 ' + i + '"></a-plane>';
+		var hori = '<a-plane class="grid" color="white" width="' + width + '" height="0.2" rotation="-90 0 0" position="0 -0.02 ' + i + '"></a-plane>';
 		$('#grids').append(hori);
 	}
 	for (var i = -width/2.0 + 0.5; i < width/2.0; i+=0.5) {
